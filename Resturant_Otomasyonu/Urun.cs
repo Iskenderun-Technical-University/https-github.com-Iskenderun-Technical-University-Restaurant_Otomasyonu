ab80ec6c67f2874c085831ca0775fa61f5e817a1
Elif Nur Şahan <elifnsahan@outlook.com>
2023-06-03 02:14:09 +0300

    classlar oluşturuldu

---
+++ Resturant_Otomasyonu/Urun.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resturant_Otomasyonu
+{
+    public class Urun
+    {
+        public  int Id { get; set; }
+        public String Urunkodu { get; set; }
+
+        public String UrunAdi { get; set; }
+
+        public String Birimi { get; set; }
+        public decimal Fiyat { get; set; }
+
+    }
+}
---
+++ Resturant_Otomasyonu/UrunHareket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resturant_Otomasyonu
+{
+    class UrunHareket
+    {
+        public string MasaKodu { get; set; }
+        public String Urunkodu { get; set; }
+
+        public String UrunAdi { get; set; }
+
+        public String Birimi { get; set; }
+        public decimal Fiyat { get; set; }
+        public decimal Miktar { get; set; }
+
+    }
+}
