562fc9e03a24e0785c8c49d75b732e48d039c56b
Elif Nur Şahan <elifnsahan@outlook.com>
2023-06-05 01:18:56 +0300

    Son değişikliler yapıldı ve sistem çalıştırıldı.

--- Resturant_Otomasyonu/Form1.Designer.cs
+++ Resturant_Otomasyonu/Form1.Designer.cs
@@ -169,13 +169,14 @@ namespace Resturant_Otomasyonu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
             this.ClientSize = new System.Drawing.Size(1373, 643);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.urun);
             this.Controls.Add(this.masa);
             this.Name = "pageUrunler";
-            this.Text = "Form1";
+            this.Text = "Resturant ";
             this.masa.ResumeLayout(false);
             this.masa.PerformLayout();
             this.urun.ResumeLayout(false);
